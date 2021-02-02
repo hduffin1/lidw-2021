31e51d40e6de727027bed0cb1c11bebd2e1f4cec
Hayley Duffin <hduffin1@hotmail.com>
2021-02-02 14:00:45 +0000

    add favicon

--- header.php
+++ header.php
@@ -9,6 +9,14 @@
     <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/sass/slick.css" />
     <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/sass/css.css?v=010220212" />
     <script>(function(H){H.className=H.className+="js"})(document.documentElement)</script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
